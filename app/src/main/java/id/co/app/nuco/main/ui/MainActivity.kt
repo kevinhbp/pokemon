787b287f2083ecb733bfa623e6efb07db898cb5b
Kevin Putro <kevin_putro@app.co.id>
2023-03-27 09:51:26 +0700

    feat(splash): move action bar to main activity

--- app/src/main/java/id/co/app/nuco/main/ui/MainActivity.kt
+++ app/src/main/java/id/co/app/nuco/main/ui/MainActivity.kt
@@ -1,20 +1,27 @@
 package id.co.app.nuco.main.ui
 
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
 import androidx.core.net.toUri
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import id.co.app.nuco.R.id
 import id.co.app.nuco.R.string
 import id.co.app.nuco.databinding.ActivityMainBinding
 import id.co.app.nuco.main.viewModels.MainState
 import id.co.app.nuco.main.viewModels.MainViewModel
 import id.co.app.nuco.splash.ui.SplashFragmentDirections
+import id.co.app.nucocore.R
 import id.co.app.nucocore.components.blocker.BlockerViewModel
 import id.co.app.nucocore.deeplink.InternalDeepLink
 import id.co.app.nucocore.domain.entities.row.BlockerDefaultModel
@@ -34,6 +41,9 @@ class MainActivity : AppCompatActivity(), MainActNavi {
 
   private val viewModel: MainViewModel by viewModel()
   private val blockerViewModel: BlockerViewModel by viewModel()
+
+  private var menuShowing: Boolean = false
+  private var animatingMenu: Boolean = false
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -87,7 +97,7 @@ class MainActivity : AppCompatActivity(), MainActNavi {
   private fun onDestinationDefault(): Boolean {
     val nav = getNavController()
     val preventSingleTouchExitFragments = arrayListOf<Int>().apply {
-      add(id.login_fragment)
+      add(id.splash_fragment)
       add(id.home_fragment)
     }
     val destinationId = nav.currentBackStackEntry?.destination?.id ?: -1
@@ -96,9 +106,7 @@ class MainActivity : AppCompatActivity(), MainActNavi {
 
   private fun onPreventExitPage(): Boolean {
     val nav = getNavController()
-    val fragmentList = listOf(
-      id.splash_fragment
-    )
+    val fragmentList = listOf<Int>()
     val destinationId = nav.currentBackStackEntry?.destination?.id ?: -1
     return fragmentList.contains(destinationId)
   }
@@ -129,6 +137,8 @@ class MainActivity : AppCompatActivity(), MainActNavi {
   private fun setupView() {
     binding.blockerViewModel = blockerViewModel
     binding.executePendingBindings()
+
+    setupMenuView()
   }
 
   private fun observeViewModel() {
@@ -179,5 +189,69 @@ class MainActivity : AppCompatActivity(), MainActNavi {
     } catch (e: Exception) {
       e.printStackTrace()
     }
+  }
+
+  // --
+  @SuppressLint("ClickableViewAccessibility")
+  private fun setupMenuView() {
+    val menuButton = binding.actionBarView.buttonMenu
+    val shadowView = binding.shadowActionBar
+    menuButton.setOnClickListener {
+      showMenuContent(!menuShowing)
+    }
+    shadowView.setOnTouchListener { _, _ ->
+      showMenuContent(!menuShowing)
+      false
+    }
+  }
+
+  private fun setMenuIcon(show: Boolean = true) {
+    val menuIcon = binding.actionBarView.imageMenuIcon
+    val menuIconDrawable = if (show) {
+      R.drawable.ic_close
+    } else {
+      R.drawable.ic_menu
+    }
+    Glide.with(menuIcon.context)
+      .load(menuIconDrawable)
+      .transition(DrawableTransitionOptions.withCrossFade())
+      .centerInside()
+      .into(menuIcon)
+  }
+
+  private fun showMenuContent(show: Boolean = true) {
+    setMenuIcon(show)
+    val menuHeight = binding.root.resources.getDimension(R.dimen.menu_height)
+    val shadowView = binding.shadowActionBar
+    val menuView = binding.actionBarView.wrapperAbLv2
+    if (menuShowing && show) return
+    if (animatingMenu) return
+    animatingMenu = true
+    menuShowing = show
+    if (show) {
+      shadowView.visibility = View.VISIBLE
+    }
+    val valueAnimator = ValueAnimator.ofInt(0, 100)
+    valueAnimator.duration = 200
+    valueAnimator.addUpdateListener {
+      val animatedValue = valueAnimator.animatedValue as Int
+      val animatedPercentage = if (show) {
+        animatedValue.toFloat() / 100f
+      } else {
+        (100f - animatedValue.toFloat()) / 100f
+      }
+      shadowView.alpha = animatedPercentage
+      val animatedHeight = (menuHeight * animatedPercentage).toInt()
+      val menuLayoutParams = menuView.layoutParams
+      menuLayoutParams.height = animatedHeight
+      menuView.layoutParams = menuLayoutParams
+    }
+    valueAnimator.doOnEnd {
+      animatingMenu = false
+      if (!show) {
+        shadowView.visibility = View.GONE
+      }
+    }
+    valueAnimator.start()
   }
 }
--- app/src/main/java/id/co/app/nuco/splash/ui/SplashFragment.kt
+++ app/src/main/java/id/co/app/nuco/splash/ui/SplashFragment.kt
@@ -1,26 +1,13 @@
 package id.co.app.nuco.splash.ui
 
-import android.animation.ValueAnimator
-import android.annotation.SuppressLint
-import android.util.Log
-import android.view.View
-import androidx.core.animation.doOnEnd
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import id.co.app.nuco.databinding.FragmentSplashBinding
 import id.co.app.nuco.splash.viewModels.SplashViewModel
 import id.co.app.nucocore.base.BaseFragment
-import id.co.app.nucocore.R.dimen
-import id.co.app.nucocore.R.drawable
-import id.co.app.nucocore.extension.toDp
-import id.co.app.nucocore.singleton.LOG_TAG
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SplashFragment : BaseFragment<FragmentSplashBinding>() {
 
   private val splashViewModel: SplashViewModel by viewModel()
-  private var menuShowing: Boolean = false
-  private var animatingMenu: Boolean = false
 
   override fun provideBinding(): FragmentSplashBinding {
     return FragmentSplashBinding.inflate(layoutInflater)
@@ -29,7 +16,6 @@ class SplashFragment : BaseFragment<FragmentSplashBinding>() {
 
   override fun setupView() {
     setStatusColor(true)
-    setupMenuView()
   }
 
   override fun observeViewModel() {
@@ -40,69 +26,5 @@ class SplashFragment : BaseFragment<FragmentSplashBinding>() {
 
   override fun loadData() {
     splashViewModel.start()
-  }
-
-  // --
-  @SuppressLint("ClickableViewAccessibility")
-  private fun setupMenuView() {
-    val menuButton = binding.actionBarView.buttonMenu
-    val shadowView = binding.shadowActionBar
-    menuButton.setOnClickListener {
-      showMenuContent(!menuShowing)
-    }
-    shadowView.setOnTouchListener { _, _ ->
-      showMenuContent(!menuShowing)
-      false
-    }
-  }
-
-  private fun setMenuIcon(show: Boolean = true) {
-    val menuIcon = binding.actionBarView.imageMenuIcon
-    val menuIconDrawable = if (show) {
-      drawable.ic_close
-    } else {
-      drawable.ic_menu
-    }
-    Glide.with(menuIcon.context)
-      .load(menuIconDrawable)
-      .transition(DrawableTransitionOptions.withCrossFade())
-      .centerInside()
-      .into(menuIcon)
-  }
-
-  private fun showMenuContent(show: Boolean = true) {
-    setMenuIcon(show)
-    val menuHeight = binding.root.resources.getDimension(dimen.menu_height)
-    val shadowView = binding.shadowActionBar
-    val menuView = binding.actionBarView.wrapperAbLv2
-    if (menuShowing && show) return
-    if (animatingMenu) return
-    animatingMenu = true
-    menuShowing = show
-    if (show) {
-      shadowView.visibility = View.VISIBLE
-    }
-    val valueAnimator = ValueAnimator.ofInt(0, 100)
-    valueAnimator.duration = 200
-    valueAnimator.addUpdateListener {
-      val animatedValue = valueAnimator.animatedValue as Int
-      val animatedPercentage = if (show) {
-        animatedValue.toFloat() / 100f
-      } else {
-        (100f - animatedValue.toFloat()) / 100f
-      }
-      shadowView.alpha = animatedPercentage
-      val animatedHeight = (menuHeight * animatedPercentage).toInt()
-      val menuLayoutParams = menuView.layoutParams
-      menuLayoutParams.height = animatedHeight
-      menuView.layoutParams = menuLayoutParams
-    }
-    valueAnimator.doOnEnd {
-      animatingMenu = false
-      if (!show) {
-        shadowView.visibility = View.GONE
-      }
-    }
-    valueAnimator.start()
   }
 }
